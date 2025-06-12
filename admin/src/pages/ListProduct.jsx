5fb3ac7c8d3eafb3ace8afa47f4354a28dd5854c
yashwaje776 <wajeyash6@gmail.com>
2025-06-12 21:58:52 +0530

    Update ListProduct.jsx

--- admin/src/pages/ListProduct.jsx
+++ admin/src/pages/ListProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {AdminContext} from "./context/AdminContext";
+import { Admincontext } from '../context/Admincontext'
 
 import { assets } from '../assets/assets'
 
