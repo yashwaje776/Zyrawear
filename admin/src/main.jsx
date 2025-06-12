654719dd65f6508c3678e0813383d9e450f58001
yashwaje776 <wajeyash6@gmail.com>
2025-06-12 21:46:17 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AdmincontextProvider from './context/admincontext.jsx'
+import AdmincontextProvider from './context/Admincontext.jsx'
 
 createRoot(document.getElementById('root')).render(
  
