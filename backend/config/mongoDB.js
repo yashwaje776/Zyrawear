8b7dffd53e2077de1fe1baa3668ba94d9cc1f13c
yashwaje776 <wajeyash6@gmail.com>
2025-07-02 16:09:37 +0530

    Update mongoDB.js

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB=async()=>{
     mongoose.connection.on("connected",()=>console.log("database connected"))
    
-    await mongoose.connect(`mongodb+srv://wajeyash6:yash@cluster0.n61dv1j.mongodb.net/E-commerse?retryWrites=true&w=majority`)
+    await mongoose.connect(`${process.env.MONGODB_URL}`)
 }
 
 export default connectDB   
